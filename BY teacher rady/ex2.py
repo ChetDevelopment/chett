5f0348a9059389cb229d1ece8f42c795502b0bbb
ChetDevelopment <ka383768@gmail.com>
2025-06-10 09:46:00 +0700

    Add files via upload

---
+++ BY teacher rady/ex2.py
@@ -0,0 +1,8 @@
+students = [
+    {"id": 1, "name": "Alice", "age": 18, "class": "A", "score": 85},
+    {"id": 2, "name": "Bob", "age": 17, "class": "B", "score": 90},
+    {"id": 3, "name": "Charlie", "age": 18, "class": "C", "score": 49},
+    {"id": 4, "name": "Dara", "age": 19, "class": "A", "score": 92},
+    {"id": 5, "name": "Eve", "age": 17, "class": "C", "score": 88}
+]
+print(students[0]["name"],[1]["name"],[2]["name"],[3]["name"],[4]["name"])
